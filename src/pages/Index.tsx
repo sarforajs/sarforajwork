ddfb4098d72439ffe7e119c12c0d0e3254486d2b
sarforajs <128588277+sarforajs@users.noreply.github.com>
2025-06-05 00:32:55 +0530

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,5 +1,5 @@
 import Hero from "@/components/Hero";
-import About from "@/components/About";
+import { About } from "@/components/About";
 import Projects from "@/components/Projects";
 import Services from "@/components/Services";
 import Skills from "@/components/Skills";
