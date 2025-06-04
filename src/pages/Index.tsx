25aa4460ccdfd610fcbeec5d6df3e6cd40cfb663
sarforajs <128588277+sarforajs@users.noreply.github.com>
2025-06-05 00:14:44 +0530

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,11 +1,10 @@
-
 import { Hero } from "@/components/Hero";
 import { About } from "@/components/About";
 import { Projects } from "@/components/Projects";
 import { Services } from "@/components/Services";
 import { Skills } from "@/components/Skills";
 import { Contact } from "@/components/Contact";
-import { Navigation } from "@/components/Navigation";
+import Navigation from "@/components/Navigation"; // Changed here
 
 const Index = () => {
   return (
